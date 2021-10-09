f1f25bb5fcf47b4e4a3dee89efc50a5ca9bf568c
Philipp Flach <flach.philipp@luzi.ch>
2021-10-09 23:25:43 +0200

    Add project files.

---
+++ PythonKursEinsteiger/willkommen.py
@@ -0,0 +1,12 @@
+#Programm für die Begrüssung
+print ("Willkommen zum 'Python Kurs' ")
+print (4 + 2)
+print ("Geben Sie bitte eine Zahl ein:")
+inhalt = input()
+#inhalt ist vom Typ String
+resultat = float(inhalt) * 2
+print ("Doppelter Wert: ", resultat)
+print ("Typ von resultat: ", type(resultat))
+text = "hallo"
+grossbuchst = text.upper()
+print ("Hallo in Grossbuchstaben: ",grossbuchst)
